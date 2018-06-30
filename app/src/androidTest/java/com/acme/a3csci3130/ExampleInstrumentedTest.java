67551a5ed86b3ffdbdd93f54e74ecc0688ec8f7b
yiditheawe1 <yiditheawe1@gmail.com>
2018-06-30 12:06:50 -0300

    2nd

--- app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
@@ -2,11 +2,15 @@ package com.acme.a3csci3130;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
 import static org.junit.Assert.*;
 import android.support.test.rule.ActivityTestRule;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +34,7 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
 
     private DatabaseReference mDatabase;
@@ -45,22 +50,22 @@ public class ExampleInstrumentedTest {
         mDatabase = FirebaseDatabase.getInstance().getReference("contacts");
         mDatabase.removeValue();
     }
-    @Ignore
-    @Test
+
+    //@Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("com.acme.a3csci3130", appContext.getPackageName());
     }
     @Test
-    public void creatContact() throws InterruptedException {
+    public void a_creatContact() throws InterruptedException {
 
         Espresso.onView(withId(R.id.submitButton)).perform(click());
         Thread.sleep(1000);
         Espresso.onView(withId(R.id.name)).perform(typeText("Joey"), closeSoftKeyboard());
         //Thread.sleep(500);
 
-        Espresso.onView(withId(R.id.number)).perform(typeText("111111111"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.number)).perform(typeText("123456789"), closeSoftKeyboard());
         //Thread.sleep(500);
         Espresso.onView(withId(R.id.primary)).perform((typeText("Fisher")), closeSoftKeyboard());
         //Thread.sleep(200);
@@ -71,21 +76,31 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
-    public  void deleteContact(){
-        onData(org.hamcrest.Matchers.anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+    public  void c_deleteContact() throws InterruptedException{
+        Thread.sleep(2000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        Thread.sleep(2000);
         onView(withId(R.id.deleteButton)).perform(click());
-        onData(org.hamcrest.Matchers.anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
-        onView(withId(R.id.primary)).check(matches(withText("Processor")));
-        onData(org.hamcrest.Matchers.anything()).inAdapterView(withId(R.id.listView)).atPosition(0).equals(null);
+        Thread.sleep(2000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).equals(null);
     }
 
-    @Test
-    public  void updateContact(){
-        onData(org.hamcrest.Matchers.anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
-        onView(withId(R.id.primary)).perform(replaceText("Processor"));
+    //@Test
+    public  void b_updateContact() throws InterruptedException{
+        Thread.sleep(1000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onView(withId(R.id.number)).perform(replaceText("123456789"));
+        closeSoftKeyboard();
+        onView(withId(R.id.name)).perform(replaceText("Joey"));
+        closeSoftKeyboard();
+        onView(withId(R.id.primary)).perform(replaceText("Fisher"));
+        closeSoftKeyboard();
         onView(withId(R.id.updateButton)).perform(click());
-        onData(org.hamcrest.Matchers.anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
-        onView(withId(R.id.primary)).check(matches(withText("Processor")));
+        Thread.sleep(1000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onView(withId(R.id.number)).check(matches(withText("123456789")));
+        onView(withId(R.id.name)).check(matches(withText("Joey")));
+        onView(withId(R.id.primary)).check(matches(withText("Fisher")));
     }
 
 }
